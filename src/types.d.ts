9a1723ce8f0097b3f8f7001fefae74b22d7125a0
Masiu <ciszekmarcell@gmail.com>
2019-10-19 20:56:08 +0200

    spell fix

--- src/types.d.ts
+++ src/types.d.ts
@@ -11,3 +11,13 @@ type IState = {
   error: null;
   setCurrent: null;
 }
+
+type IActionsTypes = {
+  GET_LOGS: string;
+  ADD_LOG: string;
+  UPDATE_LOG: string;
+  REMOVE_LOG: string;
+  GET_TECHS: string;
+  ADD_TECH: string;
+  DELETE_TECH: string;
+}
