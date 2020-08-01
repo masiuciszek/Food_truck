8d506f07041b356a3a010be631ba7d3db3c01894
Masiu <ciszekmarcell@gmail.com>
2020-08-01 21:56:02 +0200

    form fixes

--- clien1.0/components/Register.form.tsx
+++ clien1.0/components/Register.form.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Btn } from "./styles/Btns";
 import {
   FormGroupForCheckBox,
@@ -13,25 +13,94 @@ interface Props {
 }
 
 const RegisterForm = ({ title }: Props) => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    password2: "",
+    gender: "female",
+  });
+
+  const { firstName, lastName, email, password, password2, gender } = formData;
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { target } = event;
+
+    const inputValue =
+      event.target.type === "checkbox"
+        ? event.target.checked
+        : event.target.value;
+
+    setFormData({ ...formData, [event.target.name]: inputValue });
+
+    console.log(gender);
+  };
+
   return (
     <FormWrapper>
       <h3>{title}</h3>
       <FormStyles>
         <FormGroup>
-          <Input type='text' name='firstName' placeholder='firstName' />
+          <Input
+            type='text'
+            name='firstName'
+            placeholder='firstName'
+            value={firstName}
+            onChange={handleChange}
+          />
         </FormGroup>
         <FormGroup>
-          <Input type='text' name='lastName' placeholder='lastName' />
+          <Input
+            type='text'
+            name='lastName'
+            placeholder='lastName'
+            value={lastName}
+            onChange={handleChange}
+          />
         </FormGroup>
         <FormGroup>
-          <Input name='email' type='email' placeholder='email' />
+          <Input
+            name='email'
+            type='email'
+            placeholder='email'
+            value={email}
+            onChange={handleChange}
+          />
         </FormGroup>
         <FormGroupForCheckBox>
-          <Input type='checkbox' name='MALE' />
-          <Input type='checkbox' name='GENDER' />
+          <Input
+            type='radio'
+            name='gender'
+            value='female'
+            checked={gender === "female"}
+            onChange={handleChange}
+          />
+          <Input
+            type='radio'
+            name='gender'
+            value='male'
+            checked={gender === "male"}
+            onChange={handleChange}
+          />
         </FormGroupForCheckBox>
         <FormGroup>
-          <Input name='password' type='password' placeholder='password' />
+          <Input
+            name='password'
+            type='password'
+            placeholder='password'
+            value={password}
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Input
+            name='password2'
+            type='password'
+            placeholder='repeat password'
+            value={password2}
+            onChange={handleChange}
+          />
         </FormGroup>
         <Btn type='submit'>Register</Btn>
       </FormStyles>
