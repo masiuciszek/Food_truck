bb9b670fdcff6f2b1276c2957c17b28a53fc3219
Masiu <ciszekmarcell@gmail.com>
2020-08-10 11:11:46 +0200

    spelling misstake

--- clien1.0/lib/parseCookies.ts
+++
@@ -1,8 +0,0 @@
-import cookie from "cookie";
-import { IncomingMessage } from "http";
-
-export function parseCookies(req: IncomingMessage) {
-  // if there is a request we are on the server , reade the header cookies
-  // otherwhiese we are on the browser and we an read the documnet cookies
-  return cookie.parse(req ? req.headers.cookie || "" : document.cookie);
-}
--- clien1.0/src/store/page/page.actions.ts
+++
@@ -1,8 +0,0 @@
-import { SetNavOpen, SetTheme } from "./page.types";
-
-export const handleNavOpen = (): SetNavOpen => {
-  return { type: "SET_NAV_OPEN" };
-};
-export const handleTheme = (theme: string): SetTheme => {
-  return { type: "SET_THEME", payload: theme };
-};
