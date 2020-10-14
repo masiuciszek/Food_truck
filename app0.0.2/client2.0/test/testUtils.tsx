9c4724d95d72e7ff651e5ccd8e974745054df6b1
Masiu <ciszekmarcell@gmail.com>
2020-10-14 20:39:22 +0200

    setting up jest

---
+++ app0.0.2/client2.0/test/testUtils.tsx
@@ -0,0 +1,35 @@
+import * as React from "react"
+import { render } from "@testing-library/react"
+import { ThemeProvider } from "styled-components"
+import { mainTheme } from "../src/utils/theme"
+import AuthProvider from "../context/authState/AuthProvider"
+import StoreProvider from "../context/storeState/StoreProvider"
+// import { ThemeProvider } from "my-ui-lib"
+// import { TranslationProvider } from "my-i18n-lib"
+// import defaultStrings from "i18n/en-x-default"
+
+const Providers: React.FC = ({ children }) => {
+  return (
+    <ThemeProvider theme={mainTheme}>
+      <AuthProvider>
+        <StoreProvider>{children}</StoreProvider>
+      </AuthProvider>
+    </ThemeProvider>
+  )
+  // return (
+  //   <ThemeProvider theme="light">
+  //     <TranslationProvider messages={defaultStrings}>
+  //       {children}
+  //     </TranslationProvider>
+  //   </ThemeProvider>
+  // )
+}
+
+const customRender = (ui, options = {}) =>
+  render(ui, { wrapper: Providers, ...options })
+
+// re-export everything
+export * from "@testing-library/react"
+
+// override render method
+export { customRender as render }
